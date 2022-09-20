77d9df723d7bb3ba6f8a1a8dc578feb3e0f78f9b
naomischlosser <naomi_schlosser@hotmail.com>
2022-09-20 15:49:50 +0100

    test for printing multiple deposits and withdrawals

--- bank-tech-test/bank.js
+++ bank-tech-test/bank.js
@@ -35,19 +35,18 @@ class Bank {
   };
 
   printAccountStatement() {
-    const header = Object.keys(this.transfers[0]).join(" || ") + '\n';
+    const header = Object.keys(this.transfers[0]).join(" || ");
     const body = this.transfers.map(this.toFormattedString);
-    const accountStatement = header + body;
+    
+    const accountStatement = header + '\n' + body.reverse().join('\n');
 
+    console.log(accountStatement)
     return accountStatement;
   };
 
-  toFormattedString(transfer) {
-    if (!isNaN(transfer.credit)) {
-      transfer.credit = transfer.credit.toFixed(2);
-    } else if (!isNaN(transfer.debit)) {
-      transfer.credit = transfer.debit.toFixed(2);
-    }
+  toFormattedString(transfer) {    
+    if (Number.isInteger(transfer.credit)) transfer.credit = transfer.credit.toFixed(2);
+    if (Number.isInteger(transfer.debit)) transfer.debit = transfer.debit.toFixed(2);
 
     transfer.balance = transfer.balance.toFixed(2);
 
--- bank-tech-test/bank.test.js
+++ bank-tech-test/bank.test.js
@@ -55,4 +55,30 @@ describe('Bank', () => {
       '20/09/2022 || 500.00 ||  || 500.00'
     );
   });
+
+  it('prints the account statement after making a deposit and withdrawal', () => {
+    const bank = new Bank();
+    bank.deposit(1000, '16/09/2022')
+    bank.withdrawal(400, '20/10/2022')
+
+    expect(bank.printAccountStatement()).toEqual(
+      'date || credit || debit || balance' + '\n' +
+      '20/10/2022 ||  || 400.00 || 600.00' + '\n' +
+      '16/09/2022 || 1000.00 ||  || 1000.00'
+    );
+  });
+
+  it('prints the account statement after making a deposit (2x) and withdrawal', () => {
+    const bank = new Bank();
+    bank.deposit(1000, '10/01/2023')
+    bank.deposit(2000, '13/01/2023')
+    bank.withdrawal(500, '14/01/2023')
+
+    expect(bank.printAccountStatement()).toEqual(
+      'date || credit || debit || balance' + '\n' +
+      '14/01/2023 ||  || 500.00 || 2500.00' + '\n' +
+      '13/01/2023 || 2000.00 ||  || 3000.00' + '\n' +
+      '10/01/2023 || 1000.00 ||  || 1000.00'
+    );
+  });
 });
