7f9b5d9a01e9f08359411b3dd23d1853b65ce425
naomischlosser <naomi_schlosser@hotmail.com>
2022-09-20 11:05:21 +0100

    added second test

--- bank-tech-test/bank.test.js
+++ bank-tech-test/bank.test.js
@@ -1,13 +1,23 @@
 const Bank = require('./bank');
 
 describe('Bank', () => {
-  it('returns a deposit date, credit, debit and balance', () => {
+  it('returns a deposit with date, credit, debit and balance', () => {
     const bank = new Bank();
     expect(bank.deposit(500, '20/09/2022')).toEqual({
       date: '20/09/2022',
       credit: 500.00,
       debit: 0,
       balance: 500.00,
+    });
+  });
+
+  it('returns a withdrawel with date, credit, debit and balance', () => {
+    const bank = new Bank();
+    expect(bank.withdrawal(500, '20/09/2022')).toEqual({
+      date: '20/09/2022',
+      credit: 0,
+      debit: 500.00,
+      balance: -500.00,
     });
   });
 });
