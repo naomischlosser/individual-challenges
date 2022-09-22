b3c29aaf56efe39c6a8955413dd9f45adce040a2
naomischlosser <naomi_schlosser@hotmail.com>
2022-09-22 16:08:31 +0100

    changed degrades to decreases in test file

--- gilded-rose-tech-test/spec/gilded_rose_spec.rb
+++ gilded-rose-tech-test/spec/gilded_rose_spec.rb
@@ -10,28 +10,28 @@ describe GildedRose do
     end
 
     context "for any general item" do
-      it "degrades the sell_in and quality when 0 < quality < 50" do
+      it "decreases the sell_in and quality when 0 < quality < 50" do
         items = [Item.new("foo", 10, 6)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq 9
         expect(items[0].quality).to eq 5
       end
 
-      it "degrades only the sell_in when quality = 0" do
+      it "decreases only the sell_in when quality = 0" do
         items = [Item.new("foo", 10, 0)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq 9
         expect(items[0].quality).to eq 0
       end
 
-      it "degrades the sell_in and quality (-2) when sell_in < 0" do
+      it "decreases the sell_in and quality (-2) when sell_in < 0" do
         items = [Item.new("foo", 0, 2)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq -1
         expect(items[0].quality).to eq 0
       end
 
-      it "degrades the sell_in and quality (-1) when sell_in < 0" do
+      it "decreases the sell_in and quality (-1) when sell_in < 0" do
         items = [Item.new("foo", 0, 1)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq -1
@@ -39,7 +39,7 @@ describe GildedRose do
       end
 
 
-      it "degrades only the sell_in when sell_in < 0 and quality = 0" do
+      it "decreases only the sell_in when sell_in < 0 and quality = 0" do
         items = [Item.new("foo", 0, 0)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq -1
