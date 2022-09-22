159d4a2a6bd43f762df965659f4e7066b00effa9
naomischlosser <naomi_schlosser@hotmail.com>
2022-09-22 20:14:49 +0100

    TDD quality is always lower than 50 for backstage passes

--- gilded-rose-tech-test/lib/gilded_rose.rb
+++ gilded-rose-tech-test/lib/gilded_rose.rb
@@ -12,10 +12,15 @@ class GildedRose
         item.quality += 1 if item.quality < 50
       when "backstage passes to a tafkal80etc concert"
         item.sell_in -= 1
-        item.quality += 1
-        item.quality += 1 if item.sell_in <= 10
-        item.quality += 1 if item.sell_in <= 5
-        item.quality = 0 if item.sell_in < 0
+        if item.sell_in.between?(6,10)
+          item.quality += 2
+        elsif item.sell_in.between?(0,5)
+          item.quality += 3
+        elsif item.sell_in < 0
+          item.quality = 0
+        else
+          item.quality = [item.quality += 1, 50].min
+        end
       when "sulfuras, hand of ragnaros"
         # never has to be sold or decrease in quality
       else
--- gilded-rose-tech-test/spec/gilded_rose_spec.rb
+++ gilded-rose-tech-test/spec/gilded_rose_spec.rb
@@ -98,6 +98,13 @@ describe GildedRose do
         expect(items[0].sell_in).to eq -1
         expect(items[0].quality).to eq 0
       end
+
+      it "decreases the only sell_in when quality = 50" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 14
+        expect(items[0].quality).to eq 50
+      end
     end
 
     context "for special item - Sulfuras, Hand of Ragnaros" do
