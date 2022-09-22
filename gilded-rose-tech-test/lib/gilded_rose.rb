5081eee14a2adb42d0822ddd714101cd1d9128bb
naomischlosser <naomi_schlosser@hotmail.com>
2022-09-22 15:31:30 +0100

    TDD fourth test

--- gilded-rose-tech-test/lib/gilded_rose.rb
+++ gilded-rose-tech-test/lib/gilded_rose.rb
@@ -16,6 +16,7 @@ class GildedRose
       else
         item.sell_in -= 1
         item.quality -= 1 if item.quality > 0
+        item.quality -= 1 if item.sell_in <= 0
       end
     end
   end
--- gilded-rose-tech-test/spec/gilded_rose_spec.rb
+++ gilded-rose-tech-test/spec/gilded_rose_spec.rb
@@ -10,18 +10,25 @@ describe GildedRose do
     end
 
     context "for any general item" do
-      it "decreases the sell_in and quality when 0 < quality < 50" do
+      it "degrades the sell_in and quality when 0 < quality < 50" do
         items = [Item.new("foo", 10, 6)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq 9
         expect(items[0].quality).to eq 5
       end
 
-      it "decreases only the sell_in when quality = 0" do
+      it "degrades only the sell_in when quality = 0" do
         items = [Item.new("foo", 10, 0)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq 9
         expect(items[0].quality).to eq 0
+      end
+
+      it "degrades the sell_in and quality (2x) when sell_in < 0" do
+        items = [Item.new("foo", 0, 5)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq -1
+        expect(items[0].quality).to eq 3
       end
     end
   end
