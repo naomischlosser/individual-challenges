ccfaa5ffcabe99f5d8e6a201fdf99eeeb1142f25
naomischlosser <naomi_schlosser@hotmail.com>
2022-09-23 13:20:10 +0100

    moving test from statement to bankAccount

--- bank-tech-test/bankAccount.test.js
+++ bank-tech-test/bankAccount.test.js
@@ -33,20 +33,75 @@ describe("integration", () => {
     );
   });
 
-  it("prints the account statement each time (2x) after making a deposit and withdrawal", () => {
-    const bankAccount = new BankAccount();
-    bankAccount.accountDeposit(1000);
+  it("prints the account statement each time (4x) after making a deposit (2x) and withdrawal (2x)", () => {
+    const statement = new Statement();
 
-    expect(bankAccount.accountStatement()).toEqual(
+    // First deposit
+    transactionsDouble = [{
+      date: new Date(date),
+      credit: 100000,
+      debit: null,
+      balance: 100000,
+    }];
+
+    expect(statement.getStatement(transactionsDouble)).toEqual(
       "date || credit || debit || balance" +
         "\n" +
         `${dateFormatted} || £1,000.00 ||  || £1,000.00`
     );
 
-    bankAccount.accountWithdrawal(400);
+    // First withdrawal
+    withdrawalDouble1 = {
+      date: new Date(date),
+      credit: null,
+      debit: 40000,
+      balance: 60000,
+    };
 
-    expect(bankAccount.accountStatement()).toEqual(
+    expect(statement.getStatement()).toEqual(
       "date || credit || debit || balance" +
+        "\n" +
+        `${dateFormatted} ||  || £400.00 || £600.00` +
+        "\n" +
+        `${dateFormatted} || £1,000.00 ||  || £1,000.00`
+    );
+
+    // Second deposit
+    depositDouble2 = {
+      date: new Date(date),
+      credit: 50000,
+      debit: null,
+      balance: 110000,
+    };
+
+    statement.addTransaction(depositDouble2);
+
+    expect(statement.getStatement()).toEqual(
+      "date || credit || debit || balance" +
+        "\n" +
+        `${dateFormatted} || £500.00 ||  || £1,100.00` +
+        "\n" +
+        `${dateFormatted} ||  || £400.00 || £600.00` +
+        "\n" +
+        `${dateFormatted} || £1,000.00 ||  || £1,000.00`
+    );
+
+    // Second withdrawal
+    withdrawalDouble2 = {
+      date: new Date(date),
+      credit: null,
+      debit: 20050,
+      balance: 89950,
+    };
+
+    statement.addTransaction(withdrawalDouble2);
+
+    expect(statement.getStatement()).toEqual(
+      "date || credit || debit || balance" +
+        "\n" +
+        `${dateFormatted} ||  || £200.50 || £899.50` +
+        "\n" +
+        `${dateFormatted} || £500.00 ||  || £1,100.00` +
         "\n" +
         `${dateFormatted} ||  || £400.00 || £600.00` +
         "\n" +
