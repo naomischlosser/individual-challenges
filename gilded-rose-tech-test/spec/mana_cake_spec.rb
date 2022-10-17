2ad5b5f729f76311c8eadb5630de974d71a59ac7
naomischlosser <naomi_schlosser@hotmail.com>
2022-10-17 15:01:25 +0100

    add describe update_quality method

--- gilded-rose-tech-test/spec/mana_cake_spec.rb
+++ gilded-rose-tech-test/spec/mana_cake_spec.rb
@@ -2,24 +2,27 @@ require 'item'
 require 'mana_cake'
 
 describe ManaCake do
-  it "decreases the sell_in and quality when 0 < quality < 50 (capitalised)" do
-    item = Item.new("Conjured Mana Cake", 10, 5)
-    ManaCake.new(item).update_quality()
-    expect(item.sell_in).to eq 9
-    expect(item.quality).to eq 3
-  end
+  
+  describe "#update_quality" do
+    it "decreases the sell_in and quality when 0 < quality < 50 (capitalised)" do
+      item = Item.new("Conjured Mana Cake", 10, 5)
+      ManaCake.new(item).update_quality()
+      expect(item.sell_in).to eq 9
+      expect(item.quality).to eq 3
+    end
 
-  it "decreases only the sell_in when quality = 0" do
-    item = Item.new("Conjured Mana Cake", 0, 0)
-    ManaCake.new(item).update_quality()
-    expect(item.sell_in).to eq -1
-    expect(item.quality).to eq 0
-  end
+    it "decreases only the sell_in when quality = 0" do
+      item = Item.new("Conjured Mana Cake", 0, 0)
+      ManaCake.new(item).update_quality()
+      expect(item.sell_in).to eq -1
+      expect(item.quality).to eq 0
+    end
 
-  it "decreases the sell_in and quality when quality = 1" do
-    item = Item.new("Conjured Mana Cake", 0, 0)
-    ManaCake.new(item).update_quality()
-    expect(item.sell_in).to eq -1
-    expect(item.quality).to eq 0
+    it "decreases the sell_in and quality when quality = 1" do
+      item = Item.new("Conjured Mana Cake", 0, 0)
+      ManaCake.new(item).update_quality()
+      expect(item.sell_in).to eq -1
+      expect(item.quality).to eq 0
+    end
   end
 end
--- gilded-rose-tech-test/spec/sulfuras_spec.rb
+++ gilded-rose-tech-test/spec/sulfuras_spec.rb
@@ -2,10 +2,13 @@ require 'item'
 require 'sulfuras'
 
 describe Sulfuras do
-  it "never increases or decreases sell_in or quality" do
-    item = Item.new("Sulfuras, Hand of Ragnaros", 10, 50)
-    Sulfuras.new(item).update_quality()
-    expect(item.sell_in).to eq 10
-    expect(item.quality).to eq 50
+
+  describe "#update_quality" do
+    it "never increases or decreases sell_in or quality" do
+      item = Item.new("Sulfuras, Hand of Ragnaros", 10, 50)
+      Sulfuras.new(item).update_quality()
+      expect(item.sell_in).to eq 10
+      expect(item.quality).to eq 50
+    end
   end
 end
