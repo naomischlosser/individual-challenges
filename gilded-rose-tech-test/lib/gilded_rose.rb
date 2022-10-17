47dbbef473c77265220567f239cedf5e83e41f89
naomischlosser <naomi_schlosser@hotmail.com>
2022-10-17 15:16:11 +0100

    inital gilded rose test

--- gilded-rose-tech-test/lib/gilded_rose.rb
+++ gilded-rose-tech-test/lib/gilded_rose.rb
@@ -1,26 +1,19 @@
 class GildedRose
-
   def initialize(items)
     @items = items
   end
 
   def update_quality()
-
     @items.each do |item|
-      fail "Quality is outside of the 0-50 range" if !item.quality.between?(0,50)
+      fail "Quality is outside of the 0-50 range" if (!item.quality.between?(0,50) && item.name != "sulfuras, hand of ragnaros")
+  
+      item.sell_in -= 1
 
-      # case item.name.downcase
-      # when "sulfuras, hand of ragnaros"
-      #   # never has to be sold or decrease in quality
-      
-      # else
-      #   item.sell_in -= 1
-      #   if (item.sell_in > 0 && item.quality > 0) || (item.sell_in <= 0 && item.quality = 1)
-      #     item.quality -= 1
-      #   elsif item.sell_in <= 0 && item.quality >= 2
-      #     item.quality -= 2
-      #   end
-      # end
+      if (item.sell_in > 0 && item.quality > 0) || (item.sell_in <= 0 && item.quality = 1)
+        item.quality -= 1
+      elsif item.sell_in <= 0 && item.quality >= 2
+        item.quality -= 2
+      end
     end
   end
 end
--- gilded-rose-tech-test/spec/sulfuras_spec.rb
+++ gilded-rose-tech-test/spec/sulfuras_spec.rb
@@ -5,10 +5,10 @@ describe Sulfuras do
 
   describe "#update_quality" do
     it "never increases or decreases sell_in or quality" do
-      item = Item.new("Sulfuras, Hand of Ragnaros", 10, 50)
+      item = Item.new("Sulfuras, Hand of Ragnaros", 10, 80)
       Sulfuras.new(item).update_quality()
       expect(item.sell_in).to eq 10
-      expect(item.quality).to eq 50
+      expect(item.quality).to eq 80
     end
   end
 end
